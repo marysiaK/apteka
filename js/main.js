5f4d36e1ac53f4bd1142737d0959548af0d79a43
marysiaK <marysia.kolaczynska@gmail.com>
2017-04-24 20:49:36 +0200

    basic project structures + mainNav

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+    
+    // mobile navigation
+    $('.hamburger-menu').click(function() { 
+        $('.main-nav').toggleClass('mobile-nav--open');
+    });
+
+    $('.main-nav__link').click(function() { 
+        $('.main-nav').removeClass('mobile-nav--open');
+    });
+
+    // scroll reveal
+    window.sr = ScrollReveal();
+    sr.reveal('.page-section', { duration: 1000, distance: '50px' });
+
+    // smooth scroll
+
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+ });
